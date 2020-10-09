0c1404cd0bcdd5098448708570600a353ee684c5
Nguyen Thanh Dat <dat@pop-os.localdomain>
2020-10-10 00:50:39 +0700

    Initialize

---
+++ config/index.js
@@ -0,0 +1,27 @@
+'use strict'
+const path = require('path')
+
+module.exports = {
+  dev: {
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+
+    // Various Dev Server settings
+    host: 'localhost',
+    port: 8080,
+
+    // skipping other options as they are only convenience features
+  },
+  build: {
+    // Template for index.html
+    index: path.resolve(__dirname, '../dist/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+
+    productionSourceMap: true,
+  },
+}
